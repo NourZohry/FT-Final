6d9039ae2c97ea9c550ee8fe83f500d880c9fe28
NourZohry <nzohry@gmail.com>
2023-07-07 14:41:58 +0300

    Add Card Modal Dropdown for Status

--- src/components/CardModal/CardModal.js
+++ src/components/CardModal/CardModal.js
@@ -2,61 +2,109 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setClosed, getIsOpen } from "../../slices/modalSlice";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import { InputLabel, MenuItem, Select, Container, Modal, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Paper, Button, Typography, Box, Drawer, Switch } from "@mui/material";
+import { FormControl, InputLabel, MenuItem, Select, Container, Modal, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Paper, Button, Typography, Box, Drawer, Switch } from "@mui/material";
+
+const MODAL_HEIGHT = 450;
 
 export const CardModal = () => {
   const dispatch = useDispatch();
 
   const card = useSelector((state) => state.cards.selected);
+  const open = useSelector(getIsOpen);
+
+
+  const [status, setStatus] = React.useState('');
+  const handleChange = (event) => {
+    setStatus(event.target.value);
+  };
+
+  const lists = useSelector((state) => state.lists.contents);
+  // console.log(lists);
+
+  const [listOfCard, setListOfCard] = React.useState()
+
+  React.useEffect(() => {
+    if (card)
+    fetch("https://api.trello.com/1/cards/"+card.id+"/list?key=c7402336c002e9d44024966d4591bd29&token=ATTA859fe62b508ce78f6c665b7ca8298d724597956bcedb673e4ffc1bac284faeb8F9C234F6")
+      .then((res) => res.json())
+      .then((data) => setListOfCard(data));
+  }, [card]);
+
 
   return (
-    <Modal
-      open={useSelector(getIsOpen)}
-      onClose={() => dispatch(setClosed())}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
-    >
-        <Box sx={{display: "flex", alignItems: "center", justifyContent: "center"}}>
-      <Box
-        p={3}
-        sx={{ backgroundColor: "background.default" }}
-        maxWidth={"50%"}
-      >
-        <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", gap: "10px" }}>
-          <Typography
-            fontSize={"18px"}
-            fontWeight={"700"}
-          >
-            {true && card.name}
-          </Typography>
-          <MoreVertIcon sx={{ color: "secondary.dark" }} />
-        </Box>
-        <Typography
-          id="modal-modal-description"
-          sx={{ mt: 2 }}
-          color={"secondary.dark"}
-          fontSize={"13px"}
+    <>
+      {card && (
+        <Modal
+          open={open}
+          onClose={() => dispatch(setClosed())}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+          // sx={{width: "fit-content", marginX: "auto", verticalAlign: "middle"}}
+          // sx={{width: "480px", position: "absolute", marginX :"auto"}}
+          // sx={{width: "fit-content", marginX: "auto"}}
+          sx={{ position: "fixed", marginX: "auto", width: "fit-content", top: MODAL_HEIGHT / 5 }}
         >
-          {true && card.desc}
-        </Typography>
+          <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+            <Box
+              p={3}
+              sx={{ backgroundColor: "background.default" }}
+              // maxWidth={"50%"}
+              width="450px"
+              height={MODAL_HEIGHT}
+              // width= "40vw"
+              // height="70vh"
+              // minWidth="400px"
+              // minHeight="300px"
+            >
+              <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", gap: "10px" }}>
+                <Typography
+                  fontSize={"18px"}
+                  fontWeight={"700"}
+                >
+                  {true && card.name}
+                </Typography>
+                <MoreVertIcon sx={{ color: "secondary.dark" }} />
+              </Box>
+              <Typography
+                id="modal-modal-description"
+                sx={{ mt: 2 }}
+                color={"secondary.dark"}
+                fontSize={"13px"}
+              >
+                {true && card.desc}
+              </Typography>
 
-        <Typography
-          id="modal-modal-description"
-          sx={{ mt: 2 }}
-          color={"secondary.dark"}
-          fontSize={"12px"}
-          fontWeight={"700"}
-        >
-          {true && "Current Status"}
-        </Typography>
-
-      </Box>
-      </Box>
-    </Modal>
+              <Typography
+                id="modal-modal-description"
+                sx={{ mt: 2 }}
+                color={"secondary.dark"}
+                fontSize={"12px"}
+                fontWeight={"700"}
+              >
+                Current Status
+              </Typography>
+              <FormControl fullWidth>
+                <InputLabel shrink={false} sx={{opacity: status ? 0 : 100}}>{listOfCard.name}</InputLabel>
+                <Select
+                  value={status}
+                  defaultValue={listOfCard && listOfCard.name}                  
+                  onChange={handleChange}
+                >
+                  {lists && listOfCard && lists.filter(list => list.boardId === listOfCard.boardId).map(list => {
+                    return <MenuItem value={list}>{list.name}</MenuItem>
+                  })}
+                </Select>
+              </FormControl>
+            </Box>
+          </Box>
+        </Modal>
+      )}
+    </>
   );
 };
 
-{/* <FormControl fullWidth>
+{
+  /* <FormControl fullWidth>
 <InputLabel id="demo-simple-select-label">{list.name}</InputLabel>
 <Select
 labelId="demo-simple-select-label"
@@ -69,4 +117,5 @@ onChange={handleChange}
 <MenuItem value={20}>Twenty</MenuItem>
 <MenuItem value={30}>Thirty</MenuItem>
 </Select>
-</FormControl> */}
+</FormControl> */
+}
--- src/components/PageContent/PageContent.js
+++ src/components/PageContent/PageContent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { List, ListItem, ListItemButton, ListItemIcon, ListItemText, Paper, Button, Typography, Box, Drawer, Switch } from "@mui/material";
+import { MenuItem, Menu, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Paper, Button, Typography, Box, Drawer, Switch } from "@mui/material";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { useState } from "react";
 import { ListCard } from "../ListCard/ListCard";
@@ -37,93 +37,127 @@ export const PageContent = ({ board } /*: BoardType*/) => {
 
   const cards = useSelector((state) => state.cards.contents);
 
+
+
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <>
-    <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
-      <Box
-        sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", border: "1px solid #E4EBFA", backgroundColor: "background.default" }}
-        p={2}
-      >
-        <Box>
-          <Typography
-            fontWeight="700"
-            fontSize="20px"
-          >
-            {board.name}
-          </Typography>
+      <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+        <Box
+          sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", border: "1px solid #E4EBFA", backgroundColor: "background.default" }}
+          p={2}
+        >
+          <Box>
+            <Typography
+              fontWeight="700"
+              fontSize="20px"
+            >
+              {/* {board.name} */}
+            </Typography>
+          </Box>
+
+          <Box sx={{ display: "flex", alignItems: "center", gap: "10px" }}>
+            <Button variant="contained"
+                    id="basic-button"
+                    aria-controls={open ? 'basic-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                    onClick={handleClick}
+                    >+ Add New Task</Button>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-button",
+              }}
+            >
+              <MenuItem onClick={handleClose}>Edit Board</MenuItem>
+              <MenuItem onClick={handleClose}>Delete Board</MenuItem>
+            </Menu>
+            <MoreVertIcon sx={{ color: "secondary.dark" }} />
+          </Box>
         </Box>
 
-        <Box sx={{ display: "flex", alignItems: "center", gap: "10px" }}>
-          <Button variant="contained">+ Add New Task</Button>
-          <MoreVertIcon sx={{ color: "secondary.dark" }} />
-        </Box>
-      </Box>
-
-      {isLoading === true && (
-        <Box sx={{ flexGrow: 1, backgroundColor: "secondary.main", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", gap: "20px" }}>
-          <Typography
-            sx={{ color: "secondary.dark" }}
-            fontWeight="600"
-          >
-            Loading...
-          </Typography>
-        </Box>
-      )}
-
-      {isLoading === false && contents.length === 0 && (
-        <Box sx={{ flexGrow: 1, backgroundColor: "secondary.main", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", gap: "20px" }}>
-          <Typography
-            sx={{ color: "secondary.dark" }}
-            fontWeight="600"
-          >
-            This board is empty. Create a new column to get started.
-          </Typography>
-          <Button variant="contained">+ Add New Column</Button>
-        </Box>
-      )}
-
-      {/* {console.log(Object.keys(cards))} */}
-      {isLoading === false && contents.length !== 0 && (
-        <Box sx={{ flexGrow: 1, backgroundColor: "secondary.main", display: "flex", gap: "10px" }}>
-          {contents.map((list, i) => {
-            return (
-              <Box
-                sx={{ display: "flex", flexDirection: "column", gap: "10px" }}
-                p={1}
-              >
-                <Box sx={{ display: "flex", alignItems: "start", justifyContent: "start", gap: "5px" }}>
-                  <Box sx={{ width: "10px", height: "10px", backgroundColor: "red", borderRadius: "50%", flexShrink: 0, marginTop: "5px" }}></Box>
-
-                  <Typography
-                    sx={{ textTransform: "uppercase", color: "secondary.dark" }}
-                    fontSize="12px"
-                    letterSpacing={"1px"}
-                    key={i}
-                    display="inline"
-                  >
-                    {list.name} {"("}
-                    {cards && calculateCardCountForList(cards, list.id)}
-                    {")"}
-                  </Typography>
-                </Box>
-                <ListCard list={list} />
-              </Box>
-            );
-          })}
-          <Box mt={4.5} mb={4} mr={2} sx={{ borderRadius: "6px", width: "200px", maxWidth: "200px", flexGrow: 1, backgroundColor: "lightgreen", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+        {isLoading === true && (
+          <Box sx={{ flexGrow: 1, backgroundColor: "secondary.main", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", gap: "20px" }}>
             <Typography
               sx={{ color: "secondary.dark" }}
               fontWeight="600"
             >
-              + New Column
+              Loading...
             </Typography>
           </Box>
-        </Box>
-      )}
-    </Box>
+        )}
 
+        {isLoading === false && contents.length === 0 && (
+          <Box sx={{ flexGrow: 1, backgroundColor: "secondary.main", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", gap: "20px" }}>
+            <Typography
+              sx={{ color: "secondary.dark" }}
+              fontWeight="600"
+            >
+              This board is empty. Create a new column to get started.
+            </Typography>
+            <Button variant="contained">+ Add New Column</Button>
+          </Box>
+        )}
 
-    <CardModal />
+        {/* {console.log(Object.keys(cards))} */}
+        {isLoading === false && contents.length !== 0 && (
+          <Box sx={{ flexGrow: 1, backgroundColor: "secondary.main", display: "flex", gap: "10px" }}>
+            {contents.map((list, i) => {
+              return (
+                <Box
+                  sx={{ display: "flex", flexDirection: "column", gap: "10px" }}
+                  p={1}
+                >
+                  <Box sx={{ display: "flex", alignItems: "start", justifyContent: "start", gap: "5px" }}>
+                    <Box sx={{ width: "10px", height: "10px", backgroundColor: "red", borderRadius: "50%", flexShrink: 0, marginTop: "5px" }}></Box>
+
+                    <Typography
+                      sx={{ textTransform: "uppercase", color: "secondary.dark" }}
+                      fontSize="12px"
+                      letterSpacing={"1px"}
+                      key={i}
+                      display="inline"
+                    >
+                      {list.name} {"("}
+                      {cards && calculateCardCountForList(cards, list.id)}
+                      {")"}
+                    </Typography>
+                  </Box>
+                  <ListCard list={list} />
+                </Box>
+              );
+            })}
+            <Box
+              mt={4.5}
+              mb={4}
+              mr={2}
+              sx={{ borderRadius: "6px", width: "200px", maxWidth: "200px", flexGrow: 1, backgroundColor: "lightgreen", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}
+            >
+              <Typography
+                sx={{ color: "secondary.dark" }}
+                fontWeight="600"
+              >
+                + New Column
+              </Typography>
+            </Box>
+          </Box>
+        )}
+      </Box>
+
+      <CardModal />
     </>
   );
 };
